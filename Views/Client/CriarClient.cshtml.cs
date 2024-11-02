9ea3aed787b7436cbacd3c09d629d5fe1c559b1d
sil-barbosa <ma.dosil82@gmail.com>
2024-11-02 13:09:19 -0300

    Teste  homologação

---
+++ Views/Client/CriarClient.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jmsmanutencao.Views.Client
+{
+    public class CriarClientModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
